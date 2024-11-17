365c9efd728ce09a46bd9bb26df1ea417c51fe0a
luisserranom <luidserranomiranda@gmail.com>
2024-11-17 17:40:08 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// index.js
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Configurar Express para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Iniciar el servidor en el puerto 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
